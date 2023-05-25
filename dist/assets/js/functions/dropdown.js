c3971dd0043e0e3bd68c3979bc18d64ca2588ce4
Bakhriev <legendofpain5112@gmail.com>
2023-05-25 12:47:45 +0500

    header not responsive

---
+++ dist/assets/js/functions/dropdown.js
@@ -0,0 +1,14 @@
+export const dropdownInit = () => {
+  document.addEventListener('click', e => {
+    let currentDropdown
+    if (e.target.closest('[data-dropdown]')) {
+      currentDropdown = e.target.closest('[data-dropdown]')
+      currentDropdown.classList.toggle('active')
+    }
+
+    document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
+      if (dropdown === currentDropdown) return
+      dropdown.classList.remove('active')
+    })
+  })
+}
--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,8 +1,4 @@
 import './functions/burger.js'
-import { rippleEffect } from './functions/ripple-btn.js'
-import { magnifierFunc } from './functions/magnifier.js'
+import { dropdownInit } from './functions/dropdown.js'
 
-// const button = document.querySelector('.btn-r')
-// button.addEventListener('click', rippleEffect)
-
-magnifierFunc()
+dropdownInit()
